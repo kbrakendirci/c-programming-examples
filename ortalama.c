6ffd195b7cfb7c015cb8d7a9f6b8221aae19407d
Kübra kendirci <43795927+kbrakendirci@users.noreply.github.com>
2019-06-08 22:07:15 +0300

    ortalama hesaplama
    klavyeden girilen 3 sayının ortalamasını hesaplayan kod

---
+++ ortalama.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float a,b,c,ortalama;
+   printf("3 tane sayi giriniz:");
+   scanf("%f%f%f",&a,&b,&c);
+   ortalama=(a+b+c)/3;
+   printf("ortalama=%f",ortalama) ;
+   return 0;
+
+}
