b71d520399ff03cb585b0ecbfd73564cf37161ec
Kübra kendirci <43795927+kbrakendirci@users.noreply.github.com>
2019-06-08 22:18:29 +0300

    dairenin alanı

---
+++ daireninalani.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pi 3.14
+
+int main()
+{
+   float daireninalani,r;
+    printf("yarýcap giriniz=");
+    scanf("%f",&r);
+    daireninalani=pi*r*r;
+    printf("daienin alai=%f",daireninalani);
+    return 0;
+}
